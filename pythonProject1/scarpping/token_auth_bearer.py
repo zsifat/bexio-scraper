947e35618d01276c9c68c12e251ae23a60e3ac1e
zsifat <zahidulislamsifat1@gmail.com>
2024-09-11 16:30:35 +0600

    Delete pythonProject1 directory

--- pythonProject1/scarpping/token_auth_bearer.py
+++
@@ -1,9 +0,0 @@
-from requests.auth import AuthBase
-class TokenAuth(AuthBase):
-
-    def __init__(self,token):
-        self.token=token
-
-    def __call__(self, request):
-        request.headers["Authorization"] = f"Bearer {self.token}"
-        return request
